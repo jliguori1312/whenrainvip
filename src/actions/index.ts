bab5e16b90a25cf062e518e2ad129999b9f0b7da
Jeremy Liguori <jdliguori96@gmail.com>
2024-12-16 11:41:39 -0800

    add action to get weather data

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,16 +1,63 @@
 import { defineAction } from 'astro:actions'
-import { z } from 'astro:schema'
+import { object, z } from 'astro:schema'
+
+const getWhenRain = async (lat: number, long: number) => {
+    const headers = {
+        headers: {
+            'User-Agent': 'whenrainvip'
+        }
+    }
+    const pointsRes = await fetch(`https://api.weather.gov/points/${lat},${long}`, headers)
+    const pointsData = await pointsRes.json()
+
+    const url = pointsData.properties.forecast
+    const forecastRes = await fetch(url, headers)
+    const forecastData = await forecastRes.json()
+
+    const periods = forecastData.properties.periods
+    const nextRain = periods.find(period =>
+        period.probabilityOfPrecipitation.value &&
+        period.probabilityOfPrecipitation.value > 50
+    )
+    if (nextRain) {
+        return {
+            willRain: true,
+            when: nextRain.name
+        }
+    }
+    return {
+        willRain: false,
+        when: null
+    }
+}
 
 export const server = {
     getForecast: defineAction({
-        input: z.object({
-            zip: z.string(),
-        }),
+        input: z.discriminatedUnion('type', [
+            z.object({
+                type: z.literal('zip'),
+                zip: z.string(),
+            }),
+            z.object({
+                type: z.literal('coords'),
+                coords: z.object({
+                    lat: z.number(),
+                    long: z.number()
+                })
+            })
+        ]),
         handler: async (input) => {
-            //const geoData = await getCoordsFromZip(input.zip)
-            //getForecast(geoData.latt,geoData.longt)
+            if (input.type === 'zip') {
+                const geoRes = await fetch(`https://geocode.xyz/${input.zip},US?json=1`)
+                const { latt: lat, longt: long } = await geoRes.json()
+                return await getWhenRain(lat, long)
+            }
+            if (input.type === 'coords') {
+                const { lat, long } = input.coords
+                return await getWhenRain(lat, long)
+            }
 
-            return 'this is a zip test'
+            return 'bad request'
         }
         
     }),
